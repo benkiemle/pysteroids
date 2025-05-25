bc72fc28b7c809ec6b3d5c90d36b948b0e73f325
benkiemle <benkiemle@gmail.com>
2025-05-25 16:41:24 -0400

    Added player ship

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
 
@@ -11,6 +12,11 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -18,7 +24,10 @@ def main():
 
         screen.fill((0, 0, 0))
 
+        player.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
